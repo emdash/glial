0c1f9d9aa29ddb8395873fc33b311236bdad6dad
Brandon Lewis <dotsony@gmail.com>
2018-07-27 11:19:41 -0700

    Make the triangle move

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 #[macro_use]
 extern crate glium;
 
+use std::time::Instant;
+
+// Wrapper around somewhat obnoxious system time api.
+struct Clock {
+    instant: Instant,
+}
+
+impl Clock {
+    pub fn new() -> Clock {
+        Clock {
+            instant: Instant::now(),
+        }
+    }
+
+    // Return system time as floating point value.
+    pub fn seconds(&self) -> f32 {
+        let e = self.instant.elapsed();
+        ((e.as_secs() as f64) + (0.001 * e.subsec_millis() as f64)) as f32
+    }
+}
+
 #[derive(Copy, Clone)]
 struct Vertex {
     position: [f32; 2],
@@ -18,8 +39,9 @@ const vertex3: Vertex = Vertex {position: [ 0.5, -0.25]};
 const vertex_shader_src: &str = r#"
   #version 140
   in vec2 position;
+  uniform float time;
   void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
+    gl_Position = vec4(position.x + time * 0.1, position.y, 0.0, 1.0);
   }
 "#;
 
@@ -31,15 +53,17 @@ const fragment_shader_src: &str = r#"
   }
 "#;
 
-
 fn main() {
     use glium::glutin;
     use glium::Surface;
 
     let mut events_loop = glutin::EventsLoop::new();
+    let mut closed = false;
+
     let window = glutin::WindowBuilder::new();
     let context = glutin::ContextBuilder::new();
     let display = glium::Display::new(window, context, &events_loop).unwrap();
+    let clock = Clock::new();
 
     let shape = vec!{vertex1, vertex2, vertex3};
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
@@ -50,15 +74,16 @@ fn main() {
         fragment_shader_src, None
     ).unwrap();
 
-    let mut closed = false;
+
     while !closed {
         let mut target = display.draw();
+
         target.clear_color(0.0, 0.0, 1.0, 1.0);
         target.draw(
             &vertex_buffer,
             &indices,
             &program,
-            &glium::uniforms::EmptyUniforms,
+            &uniform!{time: clock.seconds()},
             &Default::default()
         ).unwrap();
 
